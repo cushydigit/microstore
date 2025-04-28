3854e56fe9186bfd582e9097dd117381f1f0431c
Shahin Rahimi <sh.rahimi.dev@gmail.com>
2025-04-28 20:25:06 +0330

    add helpers functions

---
+++ auth-service/internal/helpers/helpers.go
@@ -0,0 +1,68 @@
+package helpers
+
+import (
+	"encoding/json"
+	"errors"
+	"io"
+	"net/http"
+)
+
+type jsonResponse struct {
+	Error bool `json:"error"`
+	Message string `json:"message"`
+	Data any `json:"data,omitempty"`
+}
+
+
+func ReadJSON(w http.ResponseWriter, r*http.Request, data any) error {
+	maxBytes := 1024 * 1024 // one megabyte
+
+	r.Body = http.MaxBytesReader(w, r.Body, int64(maxBytes))
+
+	dec := json.NewDecoder(r.Body)
+	if err := dec.Decode(data); err != nil {
+		return err
+	}
+
+	if err := dec.Decode(&struct{}{}); err != io.EOF {
+		return errors.New("body must have only a single JSON value")
+	}
+
+	return nil
+}
+
+func WriteJSON(w http.ResponseWriter, status int, data any, headers ...http.Header) error {
+	out, err := json.Marshal(data)
+	if err != nil {
+		return err
+	}
+
+	if len(headers) > 0 {
+
+		// add or overwrite with custok headers
+		for key, value := range headers[0] {
+			w.Header()[key] = value	
+		}
+
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(status)
+	if _, err := w.Write(out); err != nil {
+		return err
+	}
+	return nil
+}
+
+func ErrorJSON(w http.ResponseWriter, err error, status ...int) error {
+	statusCode := http.StatusBadRequest
+	if len (status) > 0 {
+		statusCode = status[0]
+	}
+
+	var payload jsonResponse
+	payload.Error = true
+	payload.Message = err.Error()
+
+	return WriteJSON(w, statusCode, payload)
+}
